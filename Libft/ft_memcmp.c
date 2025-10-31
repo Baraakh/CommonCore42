5a853b78e28d261a96182d85fab368d18ea3aa79
Baraa Khilo <110179052+Baraakh@users.noreply.github.com>
2025-10-31 10:25:30 +0400

    Fixed some bugs wrote a test file and add a new test target to make file

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -24,7 +24,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (n > 0)
 	{
 		if (*str1 != *str2)
-			return ((unsigned char)*str1 - (unsigned char)*str2);
+			return (*str1 - *str2);
 		str1++;
 		str2++;
 		n--;
